071196ee18bf3a1d801572864fca53642d956503
wangwei <mrking_2010@126.com>
2014-12-14 22:28:49 +0800

    new version

--- src/main/java/com/topgether/base/CbVersion.java
+++ src/main/java/com/topgether/base/CbVersion.java
@@ -6,18 +6,10 @@ public class CbVersion implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
-	private String version;
+	public static String version;
 	
-	public CbVersion () {
-		this.version = System.currentTimeMillis()+"";
+	static {
+		version = System.currentTimeMillis()+"";
 	}
 
-	public String getVersion() {
-		return version;
-	}
-
-	public void setVersion(String version) {
-		this.version = version;
-	}
-	
 }
